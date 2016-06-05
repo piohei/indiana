e1a4fa302088d8d394a461b3b30c697bec8298fe
Piotr Heilman <piotr.heilman@copper.io>
2016-06-05 19:25:47 +0200

    Added site for providing location with mac address (fingerprints)

--- Indiana-WebApp/app.py
+++ Indiana-WebApp/app.py
@@ -1,4 +1,5 @@
 import time
+import requests
 
 from tornado import web, websocket, ioloop, gen
 
@@ -8,25 +9,45 @@ class IndexHandler(web.RequestHandler):
         self.render("static/index.html")
 
 
-ide = 1
+class FingerTipHandler(web.RequestHandler):
+    def get(self):
+        self.render("static/finger_tip.html")
+
+    def post(self):
+        mac = self.get_argument('mac', '')
+        x = self.get_argument('x', '')
+        y = self.get_argument('y', '')
+        z = self.get_argument('z', '')
+
+        requests.post("http://localhost:8887/", json={
+                "MAC": mac,
+                "x": x,
+                "y": y,
+                "z": z,
+            })
+
+        self.redirect("/finger_tip")
 
 
 class SocketHandler(websocket.WebSocketHandler):
     @gen.engine
     def open(self):
-        i = 0
-        global ide
-        this_id = ide
-        ide += 1
         while not self._on_close_called:
-            self.write_message("{}:{}".format(i, this_id))
-            i += 1
+            response = requests.get("http://localhost:8886/location/11:12:13:14:15:16")
+
+            if response.status_code == 200:
+                location = response.json()
+                self.write_message("{}:{}:{}".format(location["x"], location["y"], location["z"]))
+            else:
+                self.write_message("ERROR")
+
             yield gen.Task(ioloop.IOLoop.instance().add_timeout, time.time() + 1)
 
 
 if __name__ == "__main__":
     app = web.Application([
         (r"/", IndexHandler),
+        (r"/finger_tip", FingerTipHandler),
         (r"/static/(.*)", web.StaticFileHandler, {"path": "./static"}),
         (r"/websocket", SocketHandler)
     ])
