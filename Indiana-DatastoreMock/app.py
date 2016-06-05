911ad94a9aa12ce7f6d639d52185e05c43fda65f
Piotr Heilman <piotr.heilman@copper.io>
2016-06-05 14:21:05 +0200

    Run modules on different ports.

--- Indiana-DatastoreMock/app.py
+++ Indiana-DatastoreMock/app.py
@@ -39,4 +39,4 @@ def post_location(MAC):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8886)
--- Indiana-FingerTip/app.py
+++ Indiana-FingerTip/app.py
@@ -10,4 +10,4 @@ def post_location():
     return "ok"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8887)
