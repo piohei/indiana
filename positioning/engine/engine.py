75acd34106ff26f80e538f1e860c4c0fee0dc253
Damian Sepielak <damian.sepielak@gmail.com>
2016-09-14 01:42:48 +0200

    typo fixed

--- positioning/engine/engine.py
+++ positioning/engine/engine.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import chaines
+import positioning.chains as chains
+
 
 class EngineException(Exception):
     def __init__(self, message):
         self.message = message
-        
+
+
 class Engine:
-    CHAINES = {
-      'alpha': chaines.Alpha(),
+    CHAINS = {
+      'alpha': chains.Alpha(),
     }
 
     def __init__(self, chain=None):
         self.chain = chain if chain is not None else 'alpha'
 
-        if self.chain not in self.CHAINES.keys():
-            raise EngineException("Uknown chain: {}".format(self.chain))
+        if self.chain not in self.CHAINS.keys():
+            raise EngineException("Unknown chain: {}".format(self.chain))
 
     def calculate(self, *args):
-        return self.CHAINES[self.chain].calculate(*args)
+        return self.CHAINS[self.chain].calculate(*args)
