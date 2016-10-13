677044682876f822141f9fb7bfbb60680c5240c1
Piotr Heilman <piotr.heilman@copper.io>
2016-10-13 18:35:30 +0200

    Removed empty test files.

--- test/fingertip/handlers/test_socket_handler.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import unittest
-
-class TestSocketHandler(unittest.TestCase):
-    #no out of the box testing in tornado - leaving it now
-    pass
-
-if __name__ == '__main__':
-    unittest.main()
