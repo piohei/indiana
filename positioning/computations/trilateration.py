e996ba670d641f858243abe11f8294eef40d4528
Damian Sepielak <damian.sepielak@gmail.com>
2016-11-17 14:45:18 +0100

    fix in minimised function, accuracy acceptable as for global coefficients

--- positioning/computations/trilateration.py
+++ positioning/computations/trilateration.py
@@ -13,11 +13,11 @@ class Trilateration(object):
             xys = np.array([[xs[0], xs[1], -2]] * size)
             dif = pos_mtrx - xys
             squared_sum = np.square(dif).sum(1)
-            return (squared_sum - dists_squared).sum()
+            return np.abs((squared_sum - dists_squared)).sum()
 
         # res = differential_evolution(fun, ((0, 33), (0, 12)))
-        res = basinhopping(fun, [3, 5])
-        # res = minimize(fun, [0, 0])
+        # res = basinhopping(fun, [3, 5])
+        res = minimize(fun, [3, 5], bounds=[(0, 33), (0, 12)])
         print(res.message)
         return Location(float(res.x[0]), float(res.x[1]), -2.0)
         # return Location(float(xs[0]), float(xs[1]), -2.0)
